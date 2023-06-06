3f1d8a6fea054b134e9c5f6a249115429227caa0
UserHAL-9000 <jonconnorrobinson94@gmail.com>
2023-06-05 19:57:19 -0700

    working app

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,92 +1,87 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
-import { useMutation } from "@apollo/client";
-import { ADD_USER } from "../utils/mutations";
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
 
-import { login } from "../utils/auth";
-
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
       [name]: value,
     });
   };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
-    <main>
-      <h4>Sign Up</h4>
-      <div>
-        {data ? (
-          <p>
-            Success! You may now head <Link to="/">back to the homepage.</Link>
-          </p>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <label for="username">Username</label>
-            <br />
-            <input
-              placeholder="Your username"
-              name="username"
-              type="text"
-              value={formState.name}
-              onChange={handleChange}
-            />
-            <br />
-            <label for="email">Email</label>
-            <br />
-            <input
-              placeholder="Your email"
-              name="email"
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <br />
-            <label for="password">Password</label>
-            <br />
-            <input
-              placeholder="******"
-              name="password"
-              type="password"
-              value={formState.password}
-              onChange={handleChange}
-            />
-            <br />
-            <button style={{ cursor: "pointer" }} type="submit">
-              Submit
-            </button>
-          </form>
-        )}
-        {error && <div>{error.message}</div>}
-      </div>
-    </main>
+    <div className="container my-1">
+      <Link to="/login">← Go to Login</Link>
+
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
 export default Signup;
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,37 +1,45 @@
-import decode from "jwt-decode";
+import decode from 'jwt-decode';
 
-export const getToken = () => {
-  return localStorage.getItem("id_token");
-};
-
-export const getProfile = () => {
-  return decode(getToken());
-};
-
-export const isTokenExpired = (token) => {
-  // Decode the token to get its expiration time that was set by the server
-  const decoded = decode(token);
-  // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
-  if (decoded.exp < Date.now() / 1000) {
-    localStorage.removeItem("id_token");
-    return true;
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
   }
-  // If token hasn't passed its expiration time, return `false`
-  return false;
-};
 
-export const loggedIn = () => {
-  const token = getToken();
-  // If there is a token and it's not expired, return `true`
-  return token && !isTokenExpired(token) ? true : false;
-};
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
 
-export const login = (idToken) => {
-  localStorage.setItem("id_token", idToken);
-  window.location.assign("/");
-};
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
 
-export const logout = () => {
-  localStorage.removeItem("id_token");
-  window.location.reload();
-};
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
